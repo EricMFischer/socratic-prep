fec509567fdd0c4c3a341ad77f193829f0fa69f3
Eric Fischer <ericf712@gmail.com>
2016-01-10 07:00:41 -0800

    First upload of socratic-prep

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/views/assets'))
+
+var hbs = require('hbs');
+ 
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.render('index');
 })
 
 app.listen(app.get('port'), function() {
